433823eea68cb05d66aa29e89892ee53745bb8da
mariaclarabarbosa <clara_estrella@protonmail.com>
2023-02-27 19:41:49 -0300

    chore: criação do projeto

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+export default function Product() {
+  const { query } = useRouter();
+  return <h1>Product: {JSON.stringify(query)}</h1>;
+}

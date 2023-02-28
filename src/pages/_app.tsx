1039205519e6344898a4c21d7a7f9e002c9fbeaa
mariaclarabarbosa <clara_estrella@protonmail.com>
2023-02-28 19:34:29 -0300

    feat: add cabeçalho

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { globalStyles } from "../styles/global";
+import LogoImage from "../assets/logo.svg";
+import Image from "next/image";
+import { Container, Header } from "../styles/pages/app";
+
+globalStyles();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={LogoImage} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
